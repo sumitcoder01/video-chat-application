ee50a4949af143b5d729f30a7c178c3e79478253
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-06-13 19:00:47 +0530

    commit: project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+
+  return (
+    <main className="flex flex-col items-center min-h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
+
