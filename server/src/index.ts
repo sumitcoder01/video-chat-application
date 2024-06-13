ae273626da4fa088881381a1a8a48157de11b241
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-06-13 23:28:23 +0530

    chore : updating package.json and enviroment  configured

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import { PORT } from './config/envConfig';
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`video chat is running on http://localhost:${PORT}`);
 });
